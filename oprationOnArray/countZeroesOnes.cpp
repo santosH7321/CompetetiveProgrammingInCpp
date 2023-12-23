85cb529bc9fcc18a4fa60bd34c30be95b371df69
Santosh Kumar <santoshkumar23kky@gmail.com>
2023-12-23 08:00:02 +0530

    Array

---
+++ oprationOnArray/countZeroesOnes.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+void countZeroOne(int arr[], int size){
+    int zeroCount = 0;
+    int oneCount = 0;
+
+    for(int i = 0; i<size; i++){
+        if(arr[i] == 0){
+            zeroCount++;
+        }
+        if(arr[i] == 1){
+            oneCount++;
+        }
+    }
+
+    cout<<"zeroCount: "<<zeroCount<<endl;
+    cout<<"oneCount: "<<oneCount<<endl;
+}
+int main(){
+    int arr[] = {0,0,0,0,1,1,1,0,1,1,0};
+    int size = 11;
+    countZeroOne(arr, size);
+}
---
+++ oprationOnArray/linearSearchUsingFunction.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+bool linearSearch(int arr[], int size, int target){
+    for(int i = 0; i<size; i++){
+        if(arr[i]==target){
+            //found
+            return true;
+        }
+    }
+    // not found
+    return false;
+}
+int main(){
+    int arr[5] = {2,4,6,8,10};
+    int size = 5;
+    int target = 8; // where it is target
+
+    bool ans = linearSearch(arr, size, target);
+
+    if(ans == 1){
+        cout<<"Target found "<<endl;
+    }
+    else{
+        cout<<"Target not found "<<endl;
+    }
+
+}
