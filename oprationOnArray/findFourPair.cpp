16b0cca824d37648cbc9429175bf35c4713111be
Santosh Kumar <santoshkumar23kky@gmail.com>
2023-12-24 22:26:57 +0530

    tripletAndFour

---
+++ oprationOnArray/findFourPair.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[] = {1,2,3,4};
+    int n = 4;
+
+    for(int i = 0; i<n; i++){
+        for(int j = 0; j<n; j++){
+            for(int k = 0; k<n; k++){
+                for(int l = 0; l<n; l++){
+                    cout<<arr[i]<<" "<<arr[j]<<" "<<arr[k]<<" "<<arr[l]<<endl;
+                }
+            }
+        }
+    }
+
+}
